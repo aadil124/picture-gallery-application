45aff0d2ee76b11fe2742d04fd6d40b7358b1e83
aadil124 <ansariaadil124@gmail.com>
2023-09-21 21:35:02 +0530

    updated navbar component

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NavbarComponent = () => {
+const NavbarComponent = ({ inputValue, setInputValue, handleSearch }) => {
   return (
-    <nav className="navbar navbar-dark bg-dark p-3">
+    <nav className="navbar bg-success-subtle bg-body-light p-4">
       <div className="container-fluid">
-        <a href="/" className="navbar-brand">
+        <a
+          href="/"
+          className="navbar-brand text-danger text-uppercase fw-bold mb-3"
+        >
           Picture Gallery Application
         </a>
-        <form className="d-flex">
+        <form className="d-flex" onSubmit={handleSearch}>
           <input
             className="form-control me-2"
             type="search"
-            placeholder="Search"
+            placeholder="Search 'food' here..."
             aria-label="Search"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
           />
-          <button className="btn btn-outline-info" type="submit">
+          <button className="btn btn-danger" type="submit">
             Search
           </button>
         </form>
