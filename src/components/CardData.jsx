b1193ee8441e42a5fbf51e1bcb2614bf10660060
aadil124 <ansariaadil124@gmail.com>
2023-09-21 21:34:16 +0530

    Update card

--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -1,19 +1,42 @@
 import React from "react";
 
-const CardData = () => {
+const CardData = ({ picturesData }) => {
   return (
-    <div class="card m-2" style={{ width: "18rem" }}>
-      <img src="..." class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="/" class="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3  row-cols-lg-4 g-4 m-2">
+      {picturesData.map((item, index) => {
+        const img = item.urls.regular;
+        const authorName = `${item.user.first_name} ${item.user.last_name}`;
+        return (
+          <div className="col" key={index}>
+            <div className="card h-130">
+              <img
+                src={img}
+                style={{
+                  width: "100%",
+                  height: "300px",
+                  objectFit: "cover",
+                }}
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">{authorName}</h5>
+                <p className="card-text">{item.alt_description}</p>
+              </div>
+              <div className=" card-footer text-center">
+                <a
+                  href={`${item.links.html}`}
+                  target="_blank"
+                  className="btn btn-primary"
+                  rel="noreferrer"
+                >
+                  Original picture
+                </a>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
